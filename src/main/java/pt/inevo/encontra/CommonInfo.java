0c636d7485e97d4b1fe7c7294192958544b94a4f
Ricardo Dias <rspd.sao.pedro@gmail.com>
2011-06-16 18:08:06 +0100

    + Add: creation of a BTree index using a path and a name as separate parameters. ! Fix: pom and characters.

--- src/main/java/pt/inevo/encontra/CommonInfo.java
+++ src/main/java/pt/inevo/encontra/CommonInfo.java
@@ -8,4 +8,9 @@ public class CommonInfo {
     public final static String CONFIG_FILE_INDEX_PROPERTY = "index";
     public final static String CONFIG_FILE_DESCRIPTORS_PROPERTY = "descriptors";
     public final static String CONFIG_FILE_DATABASE_PROPERTY = "database";
+    public final static String CONFIG_FILE_INDEX_PATH_PROPERTY = "index_path";
+    public static String CONFIG_FILE_INDEX_PATH = "./";
+
+    public final static String[] DESCRIPTORS = new String[]{"CEDD", "ColorLayout", "Dominant Color",
+            "EdgeHistogram", "FCTH", "Scalable Color"};
 }
